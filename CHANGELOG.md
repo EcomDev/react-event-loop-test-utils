807696bdb29b89c65485f6342dd88d7c63a34e55
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2018-10-30 17:15:36 +0100

    Added conditional loop control to make it possible stop loop after something has happened

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added 
+- `ConditionBasedLoopControl` that is used on every tick of the loop to stop it when condition is met
 
 [Unreleased]: https://github.com/ecomdev/react-event-loop-test-utils/compare/4b825dc642cb6eb9a060e54bf8d69288fbee4904...HEAD
 
---
+++ tests/ConditionBasedLoopControlTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Copyright © EcomDev B.V. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace EcomDev\ReactTestUtil;
+
+use PHPUnit\Framework\TestCase;
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+
+class ConditionBasedLoopControlTest extends TestCase
+{
+    /** @var LoopInterface */
+    private $loop;
+
+    /** @var ConditionBasedLoopControlFactory */
+    private $factory;
+
+    /** @var TickRecorder */
+    private $recorder;
+
+    protected function setUp()
+    {
+        $this->loop = Factory::create();
+        $this->factory = new ConditionBasedLoopControlFactory();
+        $this->recorder = new TickRecorder($this->loop);
+    }
+
+    /** @test */
+    public function stopsLoopAfterFirstTimeConditionIsMet()
+    {
+        $this->recorder->scheduleTickLabel('Tick executed');
+
+        $this->factory->create($this->loop, function () {
+            return !$this->recorder->isEmptyRecordList();
+        });
+
+        $this->loop->run();
+
+        $this->recorder->assertRecordedLabels('Tick executed');
+    }
+
+    /** @test */
+    public function stopsLoopAfterSecondWhenConditionIsNotMet()
+    {
+        $this->recorder->scheduleEveryTick(function () {
+            usleep(200000);
+        });
+
+        $this->recorder->scheduleTickLabel('Every 200ms');
+
+        $this->factory->create(
+            $this->loop,
+            function () {
+                return false;
+            }
+        );
+
+        $this->loop->run();
+
+        $this->recorder->assertRecordedLabels(
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms'
+        );
+    }
+    
+    /** @test */
+    public function allowsToSpecifyCustomTimeoutForNotMetCondition()
+    {
+        $this->recorder->scheduleEveryTick(function () {
+            usleep(200000);
+        });
+
+        $this->recorder->scheduleTickLabel('Every 200ms');
+
+        $this->factory->createWithCustomTimeout(
+            $this->loop,
+            function () {
+                return false;
+            },
+            0.5
+        );
+
+        $this->loop->run();
+
+        $this->recorder->assertRecordedLabels(
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms',
+            'Every 200ms'
+        );
+
+    }
+
+    private function scheduleEveryTick(callable $recorder)
+    {
+        $this->loop->futureTick(function () use ($recorder) {
+            $recorder();
+            $this->scheduleEveryTick($recorder);
+        });
+    }
+}
